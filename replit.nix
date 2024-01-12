3470ddc1ac3ea14e007383a462d29095b599e2ed
Erollercoaster <erollfern@gmail.com>
2024-01-12 11:41:33 +0800

    Initial Commit

---
+++ replit.nix
@@ -0,0 +1,17 @@
+{ pkgs }: {
+  deps = [
+    pkgs.sass
+    pkgs.sass
+    pkgs.sass
+    pkgs.sass
+    pkgs.sass
+    pkgs.sass
+    pkgs.rubyPackages_3_0.sass
+    pkgs.sass
+    pkgs.sass
+    pkgs.sass
+    pkgs.sass ver
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
+  ];
+}
